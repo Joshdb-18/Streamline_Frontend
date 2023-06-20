6ddccb2f42b75cb289aab722fc12e49a4e56dca1
Oluwafemi Damilola Joshua <86154185+Joshdb-18@users.noreply.github.com>
2023-06-20 14:54:28 +0000

    Remove extra spacing :white_check_mark:

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -9,7 +9,7 @@ import GitHub from "../../public/assets/Icon/github.svg"
 
 const Footer = () => {
   return (
-    <div className="bg-white-300 pt-44 pb-24">
+    <div className="bg-white-300 pt-44">
       <div className="max-w-screen-xl w-full mx-auto px-6 sm:px-8 lg:px-16 grid grid-rows-6 sm:grid-rows-1 grid-flow-row sm:grid-flow-col grid-cols-3 sm:grid-cols-12 gap-4">
         <div className="row-span-2 sm:col-span-4 col-start-1 col-end-4 sm:col-end-5 flex flex-col items-start ">
           <Streamline className="h-8 w-auto mb-6" />
@@ -46,14 +46,14 @@ const Footer = () => {
           </div>
           <p className="text-gray-400">©{new Date().getFullYear()} - Streamline</p>
         </div>
-        <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
+        {/* <div className=" row-span-2 sm:col-span-2 sm:col-start-7 sm:col-end-9 flex flex-col">
           <p className="text-black-600 mb-4 font-medium text-lg">Product</p>
           <ul className="text-black-500 ">
             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
               Blog{" "}
             </li>
           </ul>
-        </div>
+        </div> */}
         <div className="row-span-2 sm:col-span-2 sm:col-start-9 sm:col-end-11 flex flex-col">
           <p className="text-black-600 mb-4 font-medium text-lg">Engage</p>
           <ul className="text-black-500">
@@ -62,12 +62,6 @@ const Footer = () => {
             </li>
             <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
               About Us{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Privacy Policy{" "}
-            </li>
-            <li className="my-2 hover:text-orange-500 cursor-pointer transition-all">
-              Terms of Service{" "}
             </li>
           </ul>
         </div>
