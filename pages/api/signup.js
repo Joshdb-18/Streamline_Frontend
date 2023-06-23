444e6252e29784af812376cbaad2b8f3dd903de3
Oluwafemi Damilola Joshua <86154185+Joshdb-18@users.noreply.github.com>
2023-06-23 18:15:19 +0000

    Handke error message :white_check_mark:

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
       console.error(error);
 
       // Return a response indicating an error
-      return res.status(500).json({ error: 'An error occurred during signup' });
+      return res.status(500).json({ error: 'An error occurred during signup: ' + error.message});
     }
   }
 
