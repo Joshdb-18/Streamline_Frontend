1030170997fddd5abba5843c6f9335da87789fe4
Damilola Oluwafemi <joshuadami17@gmail.com>
2023-07-02 14:15:31 +0100

    fix variable declaration error :bug:

--- pages/auth/password_update.js
+++ pages/auth/password_update.js
@@ -9,17 +9,20 @@ import Avatar from '@mui/material/Avatar';
 import KeyOutlinedIcon from '@mui/icons-material/KeyOutlined';
 import Typography from '@mui/material/Typography';
 
-
 export default function PasswordUpdate() {
   const router = useRouter();
+  const [uidb64, setUidb64] = useState('');
+  const [token, setToken] = useState('');
+
   useEffect(() => {
     // Get the query parameters from the URL
     const queryParams = new URLSearchParams(router.asPath.split('?')[1]);
-    const uidb64 = queryParams.get('uidb64');
-    const token = queryParams.get('token');
-    console.log('Uidb64: ', uidb64);
-    console.log('Token: ', token);
-  });
+    const uidb64Value = queryParams.get('uidb64');
+    const tokenValue = queryParams.get('token');
+    setUidb64(uidb64Value);
+    setToken(tokenValue);
+  }, []);
+
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
@@ -53,10 +56,7 @@ export default function PasswordUpdate() {
         // Password update verification is successful
         // Display a success message or redirect to the password update page
         console.log('Password update verification successful');
-
-        // Delete uidb64 and passwordToken cookies
-        router.push('/success/password-reset-success')
-
+        router.push('/success/password-reset-success');
       } else {
         // Password update verification failed
         router.push('/error/password-reset-error');
@@ -70,14 +70,14 @@ export default function PasswordUpdate() {
   return (
     <Container component="main" maxWidth="xs">
       <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 23,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
+      <Box
+        sx={{
+          marginTop: 23,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
         <Avatar sx={{ m: 1, bgcolor: '#F53833' }}>
           <KeyOutlinedIcon />
         </Avatar>
@@ -105,18 +105,19 @@ export default function PasswordUpdate() {
           />
           {error && <Typography color="error">{error}</Typography>}
           <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3,
-                    mb: 2,
-                    bgcolor: '#F53838',
-                    '&:hover': {
-                      bgcolor: '#e53030',
-                  },
-                }}
-              >
-              Reset Password
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{
+              mt: 3,
+              mb: 2,
+              bgcolor: '#F53838',
+              '&:hover': {
+                bgcolor: '#e53030',
+              },
+            }}
+          >
+            Reset Password
           </Button>
         </form>
       </Box>
