e256b8ec4de28fbe234aaeabfc47f1c5c1ede9e7
Damilola Oluwafemi <joshuadami17@gmail.com>
2023-07-02 13:45:51 +0100

    Update package :package: and redirects :sparkles:

--- pages/api/password-reset.js
+++ pages/api/password-reset.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
         email,
       }, {
         headers: {
-          'Content-Type': 'applicatsion/json',
+          'Content-Type': 'application/json',
           'X-Requested-From': 'www.app.devnetwork.tech',
         },
       });
