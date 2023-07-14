eb2d14b7e1ae1c919c8a52e88fbcb812c6f14c9b
Oluwafemi Damilola Joshua <86154185+Joshdb-18@users.noreply.github.com>
2023-07-14 22:25:04 +0000

    Update youtube-callback.js

--- pages/api/youtube-callback.js
+++ pages/api/youtube-callback.js
@@ -1,30 +1,32 @@
 import axios from "axios";
 
 export default async function handler(req, res) {
-  const { token, state } = req.body;
+  if (req.method === "POST") {
+    const { token, state } = req.body;
 
-  // Create the Authorization header
-  const headers = {
-    Authorization: `Token ${token}`,
-  };
+    // Create the Authorization header
+    const headers = {
+      Authorization: `Token ${token}`,
+    };
 
-  if (state) {
-    try {
-      // Make a request to the backend callback URL
-      const response = await axios.get(
-        `https://backend.devnetwork.tech/api/v1/youtube/callback?state=${state}`,
-        {
-          headers: headers,
-        }
-      );
+    if (state) {
+      try {
+        // Make a request to the backend callback URL
+        const response = await axios.get(
+          `https://backend.devnetwork.tech/api/v1/youtube/callback?state=${state}`,
+          {
+            headers: headers,
+          }
+        );
 
-      console.log(response.data);
-      res.status(200).json({ success: true });
-    } catch (error) {
-      console.error("Error:", error);
-      res.status(500).json({ error: "Failed to process YouTube callback" });
+        console.log(response.data);
+        res.status(200).json({ success: true });
+      } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ error: "Failed to process YouTube callback" });
+      }
+    } else {
+      res.status(400).json({ error: "Invalid request" });
     }
-  } else {
-    res.status(400).json({ error: "Invalid request" });
   }
 }
