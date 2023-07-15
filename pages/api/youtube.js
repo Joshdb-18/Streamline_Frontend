224b1e3ff90722fb73f12125d710e80da4fd86a1
Oluwafemi Damilola Joshua <86154185+Joshdb-18@users.noreply.github.com>
2023-07-15 11:24:38 +0000

    Update youtube.js

--- pages/api/youtube.js
+++ pages/api/youtube.js
@@ -10,7 +10,7 @@ export default async function handler(req, res) {
   try {
     // Make a GET request to the backend YouTube videos endpoint
     const response = await axios.get(
-      "https://backend.devnetwork.tech/api/v1/youtube-videos/",
+      "https://backend.devnetwork.tech/api/v1/youtube/uploaded-videos/",
       {
         headers: headers,
       }
