f149283a04ebc1acf41597c1a8c36447424067b5
Oluwafemi Damilola Joshua <86154185+Joshdb-18@users.noreply.github.com>
2023-07-18 10:27:27 +0000

    Update 3 files

--- pages/user/main.js
+++ pages/user/main.js
@@ -72,7 +72,12 @@ const Dashboard = () => {
             <Streamline className="h-8 w-auto" />
           </div>
           <div className="col-start-10 col-end-12 font-medium flex justify-end items-center">
-            <ButtonOutline onClick={handleLogout}>Logout</ButtonOutline>
+            <Button
+              className="font-medium tracking-wide py-2 px-5 sm:px-8 border border-orange-500 text-orange-500 bg-white-500 outline-none rounded-l-full rounded-r-full capitalize hover:bg-orange-500 hover:text-white-500 transition-all hover:shadow-orange"
+              onClick={handleLogout}
+            >
+              Logout
+            </Button>
           </div>
         </nav>
       </header>
