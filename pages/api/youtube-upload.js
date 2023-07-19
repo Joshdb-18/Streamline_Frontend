6834952fb1652462d5fa985193a59b028cf93c2a
Oluwafemi Damilola Joshua <86154185+Joshdb-18@users.noreply.github.com>
2023-07-19 19:21:08 +0000

    Update 3 files

--- pages/api/youtube-upload.js
+++ pages/api/youtube-upload.js
@@ -1,41 +1,52 @@
 import axios from "axios";
+import formidable from "formidable";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const token = req.headers.authorization.replace("Token ", "");
 
-    const videoTitle = req.body.title;
-    const videoDescription = req.body.description;
-    const videoPrivacyStatus = req.body.privacy_status;
-    const videoFile = req.files.video;
-    const videoCategory = req.body.category;
-    const videoMadeForKids = req.body.made_for_kids === "true";
+    const form = new formidable.IncomingForm();
 
-    const apiUrl = "https://backend.devnetwork.tech/api/v1/youtube/upload/";
+    form.parse(req, async (err, fields, files) => {
+      if (err) {
+        console.error("An error occurred while parsing form data:", err);
+        res.status(400).json({ success: false, error: "Bad Request" });
+        return;
+      }
 
-    const formData = new FormData();
-    formData.append("title", videoTitle);
-    formData.append("description", videoDescription);
-    formData.append("privacy_status", videoPrivacyStatus);
-    formData.append("video", videoFile);
-    formData.append("category", videoCategory);
-    formData.append("made_for_kids", videoMadeForKids);
+      const videoTitle = fields.title;
+      const videoDescription = fields.description;
+      const videoPrivacyStatus = fields.privacy_status;
+      const videoFile = files.video;
+      const videoCategory = fields.category;
+      const videoMadeForKids = fields.made_for_kids === "true";
 
-    try {
-      const response = await axios.post(apiUrl, formData, {
-        headers: {
-          Authorization: `Token ${token}`,
-          "Content-Type": "multipart/form-data",
-        },
-      });
+      const apiUrl = "https://backend.devnetwork.tech/api/v1/youtube-upload/";
 
-      // Handle the response from the backend
-      res.status(200).json({ success: true });
-    } catch (error) {
-      // Handle any error that occurs during the video upload
-      console.error("An error occurred during video upload:", error);
-      res.status(500).json({ success: false, error: "Video upload failed" });
-    }
+      const formData = new FormData();
+      formData.append("title", videoTitle);
+      formData.append("description", videoDescription);
+      formData.append("privacy_status", videoPrivacyStatus);
+      formData.append("video", videoFile, videoFile.name);
+      formData.append("category", videoCategory);
+      formData.append("made_for_kids", videoMadeForKids);
+
+      try {
+        const response = await axios.post(apiUrl, formData, {
+          headers: {
+            Authorization: `Token ${token}`,
+            "Content-Type": `multipart/form-data; boundary=${formData._boundary}`,
+          },
+        });
+
+        // Handle the response from the backend
+        res.status(200).json({ success: true });
+      } catch (error) {
+        // Handle any error that occurs during the video upload
+        console.error("An error occurred during video upload:", error);
+        res.status(500).json({ success: false, error: "Video upload failed" });
+      }
+    });
   } else {
     res.status(405).end();
   }
