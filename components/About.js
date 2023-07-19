fe24be0dadfc770dee06dc49885c5243f55dcf7b
Oluwafemi Damilola Joshua <86154185+Joshdb-18@users.noreply.github.com>
2023-07-19 00:26:12 +0000

    Update About.js and Hero.js

--- components/About.js
+++ components/About.js
@@ -6,7 +6,7 @@ import ButtonOutline from "./misc/ButtonOutline.";
 import { motion } from "framer-motion";
 import getScrollAnimation from "../utils/getScrollAnimation";
 import ScrollAnimationWrapper from "./Layout/ScrollAnimationWrapper";
-import Link from "next/link"
+import Link from "next/link";
 
 const About = () => {
   const scrollAnimation = useMemo(() => getScrollAnimation(), []);
@@ -29,7 +29,8 @@ const About = () => {
               variants={scrollAnimation}
               className="leading-normal w-10/12 sm:w-7/12 lg:w-6/12 mx-auto my-2 text-center"
             >
-            Discover the exciting features that Streamline offers, designed to enhance your experience with seamless efficiency.
+              Discover the exciting features that Streamline offers, designed to
+              enhance your experience with seamless efficiency.
             </motion.p>
           </ScrollAnimationWrapper>
           <div className="grid grid-flow-row sm:grid-flow-col grid-cols-1 sm:grid-cols-3 gap-4 lg:gap-12 py-8 lg:py-12 px-6 sm:px-0 lg:px-6">
@@ -40,9 +41,9 @@ const About = () => {
                 whileHover={{
                   scale: 1.1,
                   transition: {
-                    duration: 0.2
-                  }
-              }}
+                    duration: 0.2,
+                  },
+                }}
               >
                 <div className="p-4 lg:p-0 mt-6 lg:mt-16">
                   <Image
@@ -57,16 +58,16 @@ const About = () => {
                 </p>
                 <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
                   <li className="relative check custom-list my-2">
-                  Social Media Sharing
+                    Social Media Sharing
                   </li>
                   <li className="relative check custom-list my-2">
-                  Login Integration
+                    Login Integration
                   </li>
                   <li className="relative check custom-list my-2">
-                  Page Integration
+                    Page Integration
                   </li>
                   <li className="relative check custom-list my-2">
-                  Messenger Integration
+                    Messenger Integration
                   </li>
                 </ul>
                 <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
@@ -86,36 +87,36 @@ const About = () => {
                 whileHover={{
                   scale: 1.1,
                   transition: {
-                    duration: 0.2
-                  }
+                    duration: 0.2,
+                  },
                 }}
               >
                 <div className="p-4 lg:p-0 mt-6 lg:mt-16">
                   <Image
-                    src="/assets/Twitter.png"
+                    src="/assets/YouTube.png"
                     width={145}
                     height={165}
-                    alt="Streamline Twitter Img"
+                    alt="Streamline YouTube Img"
                   />
                 </div>
                 <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
-                  Twitter Features
+                  YouTube Features
                 </p>
                 <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
                   <li className="relative check custom-list my-2">
-                  Trending Topics
+                    Upload Videos
                   </li>
                   <li className="relative check custom-list my-2">
-                  Real Time Monitoring
+                    Check Analytics
                   </li>
                   <li className="relative check custom-list my-2">
-                  Advanced Analytics
+                    See personal videos
                   </li>
                   <li className="relative check custom-list my-2">
-                  Scheduled Tweets
+                    View liked videos
                   </li>
                   <li className="relative check custom-list my-2">
-                  Social Listening
+                    Manage Channel
                   </li>
                 </ul>
                 <div className="flex flex-col w-full justify-center mb-8 flex-none mt-12">
@@ -135,8 +136,8 @@ const About = () => {
                 whileHover={{
                   scale: 1.1,
                   transition: {
-                    duration: 0.2
-                  }
+                    duration: 0.2,
+                  },
                 }}
               >
                 <div className="p-4 lg:p-0 mt-6 lg:mt-16">
@@ -144,7 +145,7 @@ const About = () => {
                     src="/assets/Tiktok.png"
                     width={145}
                     height={165}
-                    alt="Streamline Premium Plan"
+                    alt="Streamline TikTok img"
                   />
                 </div>
                 <p className="text-lg text-black-600 font-medium capitalize my-2 sm:my-7">
@@ -152,19 +153,19 @@ const About = () => {
                 </p>
                 <ul className="flex flex-col list-inside pl-6 xl:pl-0 items-start justify-start text-left text-black-500 flex-grow">
                   <li className="relative check custom-list my-2">
-                  TikTok Integration
+                    TikTok Integration
                   </li>
                   <li className="relative check custom-list my-2">
-                  Tiktok Scheduler
+                    Tiktok Scheduler
                   </li>
                   <li className="relative check custom-list my-2">
-                  Trends Discovery
+                    Trends Discovery
                   </li>
                   <li className="relative check custom-list my-2">
-                  Influencer Collaboration
+                    Influencer Collaboration
                   </li>
                   <li className="relative check custom-list my-2">
-                  Analytics Dashboard
+                    Analytics Dashboard
                   </li>
                   <li className="relative check custom-list my-2">
                     And much more...
@@ -186,15 +187,16 @@ const About = () => {
           <ScrollAnimationWrapper>
             <motion.h3
               variants={scrollAnimation}
-              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/12 sm: lg:w-4/12 mx-auto">
+              className="text-2xl sm:text-3xl lg:text-4xl font-medium text-black-600 leading-normal w-9/12 sm: lg:w-4/12 mx-auto"
+            >
               Trusted by Thousands of Happy Customer{" "}
             </motion.h3>
             <motion.p
               variants={scrollAnimation}
               className="leading-normal mx-auto mb-2 mt-4 w-10/12 sm:w-7/12 lg:w-6/12"
             >
-              Presenting the Testimonials of our Valued Customers Who Delightedly
-              Embraced our Remarkable Feature.
+              Presenting the Testimonials of our Valued Customers Who
+              Delightedly Embraced our Remarkable Feature.
             </motion.p>
           </ScrollAnimationWrapper>
           <ScrollAnimationWrapper className="w-full flex flex-col py-12">
@@ -203,7 +205,7 @@ const About = () => {
             </motion.div>
           </ScrollAnimationWrapper>
           <ScrollAnimationWrapper className="relative w-full mt-16">
-            <motion.div variants={scrollAnimation} custom={{duration: 3}}>
+            <motion.div variants={scrollAnimation} custom={{ duration: 3 }}>
               <div className="absolute rounded-xl  py-8 sm:py-14 px-6 sm:px-12 lg:px-16 w-full flex flex-col sm:flex-row justify-between items-center z-10 bg-white-500">
                 <div className="flex flex-col text-left w-10/12 sm:w-7/12 lg:w-5/12 mb-6 sm:mb-0">
                   <h5 className="text-black-600 text-xl sm:text-2xl lg:text-3xl leading-relaxed font-medium">
@@ -218,7 +220,7 @@ const About = () => {
               <div
                 className="absolute bg-black-600 opacity-5 w-11/12 roudned-lg h-60 sm:h-56 top-0 mt-8 mx-auto left-0 right-0"
                 style={{ filter: "blur(114px)" }}
-                ></div>
+              ></div>
             </motion.div>
           </ScrollAnimationWrapper>
         </div>
